3d2e0c0b65967de9e00a094a699c4c35cae41087
Katirina Dimitrova <katirina79@gmail.com>
2018-01-02 18:59:46 +0100

    version initial js menu button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+function init(){
+  var menu = document.querySelector('.button-menu');
+  menu.addEventListener('click', menuTrigger);
+}
+
+function menuTrigger(){
+  var menuHidden = document.querySelector('.menu');
+  menuHidden.classList.toggle('hidden');
+}
+
+init();
